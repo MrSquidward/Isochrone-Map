4186f295b49fb6d5043a5c78049a8d69afca5617
Inuen <57043599+Inuen@users.noreply.github.com>
2020-11-08 19:56:25 +0100

    Model improvements and testing algorythm

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class Node:
         self.id = (int(x), int(y))
         self.edges = e
 
-    def heuristic_cost(self, hx, hy, manhattan=True):
+    def heuristic_cost(self, hx, hy, manhattan=False):
         x_diff = abs(self.x - hx)
         y_diff = abs(self.y - hy)
 
@@ -23,17 +23,24 @@ class Node:
 
 
 class Edge:
-    def __init__(self, f_node, t_node, c, d=0):
+    def __init__(self, f_node, t_node, c, FID, d=0):
         self.from_node_id = f_node
         self.to_node_id = t_node
         self.id = (f_node, t_node)
         self.cost = c
         self.direction = d
+        self.fid = FID
 
     def get_end(self, one_node_id):
         if one_node_id == self.from_node_id:
             return self.to_node_id
         return self.from_node_id
+
+    def get_cost(self):
+        return self.cost
+
+    # def get_time_cost(self):
+    #     return self.cost /
 
 
 class Graph:
@@ -41,6 +48,51 @@ class Graph:
         self.edges = e
         self.nodes = n
 
+        self.__add_node_edges()
+
+        self.dict_of_nodes_by_id = {}
+        for node in self.nodes:
+            self.dict_of_nodes_by_id[node.id] = node
+
+        self.dict_of_edges_by_id = {}
+        for edge in self.edges:
+            self.dict_of_edges_by_id[edge.id] = edge
+
+    def __add_node_edges(self):
+        nodes_edges = {}
+        for node in self.nodes:
+            nodes_edges[node.id] = []
+
+        for edge in self.edges:
+            if edge.id not in nodes_edges[edge.from_node_id]:
+                nodes_edges[edge.from_node_id].append(edge.id)
+            if edge.id not in nodes_edges[edge.to_node_id]:
+                nodes_edges[edge.to_node_id].append(edge.id)
+
+        for node in self.nodes:
+            node.edges = nodes_edges[node.id]
+
+    def get_node_by_id(self, node_id):
+        return self.dict_of_nodes_by_id[node_id]
+
+    def get_edge_by_id(self, edge_id):
+        return self.dict_of_edges_by_id[edge_id]
+
+    def get_neighbours(self, node_id):
+        neighbour_edges = self.dict_of_nodes_by_id[node_id].edges
+        neighbour_nodes = []
+        for edge in neighbour_edges:
+            neighbour_nodes.append(self.get_edge_by_id(edge).get_end(node_id))
+
+        return_list = []
+        for index in range(0, len(neighbour_nodes)):
+            return_list.append((neighbour_nodes[index], neighbour_edges[index]))
+
+        return return_list
+
+    def get_closest_node(self, my_x, my_y):
+        final_list = sorted(self.nodes, key=lambda node: math.sqrt(abs(my_x - node.x) ** 2 + abs(my_y - node.y) ** 2))
+        return final_list[0]
 
 def extract_minimum(q, d):
     extract = {}
@@ -58,35 +110,39 @@ def pathfinding_a_star(graph, start_id, end_id):
     f_score = {}  # value of a path from the start node to the current node with heuristics
     g_score = {}  # value of a path from the start node to the current node without heuristics
     p = {}  # previous node in a path
-    all_nodes = {}  # map of all nodes in graph - key is an id of node, value is an object of node
     for node in graph.nodes:
         f_score[node.id] = sys.maxsize
         g_score[node.id] = sys.maxsize
         p[node.id] = (-1, -1)
-        all_nodes[node.id] = node
         neighbours_map[node.id] = False
 
     g_score[start_id] = 0
-    current_node = all_nodes[start_id]
-    f_score[start_id] = current_node.heuristic_cost(all_nodes[end_id].x, all_nodes[end_id].y)
+    current_node = graph.get_node_by_id(start_id)
+    f_score[start_id] = current_node.heuristic_cost(graph.get_node_by_id(end_id).x, graph.get_node_by_id(end_id).y)
     q_list.add(start_id)
     neighbours_map[start_id] = True
 
     while len(q_list) != 0:
         current_node_id = extract_minimum(q_list, f_score)  # get id of a node from q_list with the lowest path value
-        current_node = all_nodes[current_node_id]
+        current_node = graph.get_node_by_id(current_node_id)
 
         if current_node_id == end_id:
             break
 
         q_list.remove(current_node.id)
         neighbours_map[current_node.id] = False
+        neighbouring_nodes = []
+        for el in graph.get_neighbours(current_node.id):
+            neighbouring_nodes.append(el[0])
 
-        for edge in current_node.edges:
-            next_node = all_nodes[edge.to_node_id]
-            h_value = next_node.heuristic_cost(all_nodes[end_id].x, all_nodes[end_id].y)
-            tentative_g_score = g_score[current_node.id] + edge.cost
-
+        for node in neighbouring_nodes:
+            next_node = graph.get_node_by_id(node)
+            h_value = next_node.heuristic_cost(graph.get_node_by_id(end_id).x, graph.get_node_by_id(end_id).y)
+            try:
+                edge_cost = graph.get_edge_by_id((current_node.id, next_node.id)).get_cost()
+            except:
+                edge_cost = graph.get_edge_by_id((next_node.id, current_node.id)).get_cost()
+            tentative_g_score = g_score[current_node.id] + edge_cost
             if tentative_g_score < g_score[next_node.id]:
                 p[next_node.id] = current_node.id
                 g_score[next_node.id] = tentative_g_score
---
+++ testing.py
@@ -0,0 +1,31 @@
+import arcpy
+from model import *
+
+arcpy.env.workspace = r'D:\pycharm\pag_algorythm\BDOT_Torun'
+arcpy.env.overwriteOutput = True
+
+roads_fc = 'L4_1_BDOT10k__OT_SKJZ_L.shp'
+
+points = []  # list of all points
+edges = []  # list of edges
+d = {}
+
+for row in arcpy.da.SearchCursor(roads_fc, ["SHAPE@", "SHAPE@LENGTH", "FID"]):
+    f_node = Node(round(row[0].firstPoint.X), round(row[0].firstPoint.Y))
+    l_node = Node(round(row[0].lastPoint.X), round(row[0].lastPoint.Y))
+    e_length = row[1]
+    e = Edge(f_node.id, l_node.id, e_length, row[2])
+    edges.append(e)
+    d[f_node.id] = f_node
+    d[l_node.id] = l_node
+
+nodes = []  # list of nodes
+for i in d:
+    nodes.append(d[i])
+
+torun_skjz = Graph(edges, nodes)
+
+start_point = torun_skjz.get_closest_node(474015.240, 572269.582)
+end_point = torun_skjz.get_closest_node(474076.359, 572436.534)
+my_path = pathfinding_a_star(torun_skjz, start_point.id, end_point.id)
+print my_path
